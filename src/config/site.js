a75bb5c752f3de014e1ae4bf03773c1f365699aa
ZALTRON ROBOTICS <zaltronrobotics@gmail.com>
2026-09-05 19:42:50 +0530

    Add SEO metadata, robots.txt and sitemap

--- src/config/site.js
+++ src/config/site.js
@@ -1,4 +1,4 @@
 export const site = {
-  companyName: 'ZALTRON ROBOTICS', brandName: 'ZALTRON', flagship: 'ZALTRON V1', tagline: 'Building Intelligent Machines', location: 'India', email: 'zaltronrobotics@gmail.com', url: 'https://zaltronrobotics.com',
+  companyName: 'ZALTRON ROBOTICS', brandName: 'ZALTRON', flagship: 'ZALTRON V1', tagline: 'Building Intelligent Machines', location: 'India', email: 'zaltronrobotics@gmail.com', url: 'https://zaltron-robotics.zaltronrobotics.workers.dev',
   social: { YouTube: '#', Instagram: '#', Facebook: '#', LinkedIn: '#', X: '#', GitHub: '#', Reddit: '#' }
 };
