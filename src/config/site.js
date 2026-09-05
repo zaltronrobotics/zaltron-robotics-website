5263c84d41ec88a321165c59291f4f16a001a0d3
ZALTRON ROBOTICS <zaltronrobotics@gmail.com>
2026-09-05 21:40:09 +0530

    Add ZALTRON social branding and logo SEO

--- src/config/site.js
+++ src/config/site.js
@@ -1,4 +1,12 @@
 export const site = {
   companyName: 'ZALTRON ROBOTICS', brandName: 'ZALTRON', flagship: 'ZALTRON V1', tagline: 'Building Intelligent Machines', location: 'India', email: 'zaltronrobotics@gmail.com', url: 'https://zaltron-robotics.zaltronrobotics.workers.dev',
-  social: { YouTube: '#', Instagram: '#', Facebook: '#', LinkedIn: '#', X: '#', GitHub: '#', Reddit: '#' }
+  social: {
+    YouTube: 'https://www.youtube.com/@zaltronrobotics',
+    Instagram: 'https://www.instagram.com/zaltronrobotics/',
+    Facebook: 'https://www.facebook.com/profile.php?id=61594066681659',
+    LinkedIn: 'https://www.linkedin.com/in/zaltron-robotics-a67074434/',
+    X: 'https://x.com/ZaltronRobotics',
+    GitHub: 'https://github.com/zaltronrobotics',
+    Reddit: 'https://www.reddit.com/user/zaltronrobotics/'
+  }
 };
